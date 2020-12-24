f4fc974c2a8a07e9b954be5e22a2c9409a62f354
Juan III De los santos <j3delossantos@outlook.com>
2020-12-23 22:47:26 -0400

    Building Nested Components, binding, passing data.

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -5,11 +5,16 @@ import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {ProductListComponent} from './products/product-list.component';
+import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
+import{StarComponent} from './shared/star.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent
   ],
   imports: [
     BrowserModule,
