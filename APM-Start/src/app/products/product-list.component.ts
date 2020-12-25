cd57244921602de8a0213b97e6ba2ae8fc9b075c
Juan III De los santos <j3delossantos@outlook.com>
2020-12-25 16:35:57 -0400

    Navigation and routing basics

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -3,7 +3,7 @@ import { from } from 'rxjs';
 import {IProduct} from './product';
 import {ProductService} from './product.service';
 @Component({
-    selector:'pm-products',
+    //selector:'pm-products',
     templateUrl:'./product-list.component.html',
     styleUrls:['./product-list.component.css']
 })
